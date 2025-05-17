36704bbc8d0626384624306310c71b4e7178314d
huongpham4 <huong.pham4@sotatek.com>
2025-05-17 20:53:35 +0700

    [DEV][TRAIN] implement model training with callbacks and history saving

--- convert_model.py
+++ convert_model.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
 
-model = tf.keras.models.load_model('trained_model_v2.keras')
+model = tf.keras.models.load_model('trained_model_v6.keras')
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
-with open('model_v2.tflite', 'wb') as f:
+with open('model_v6.tflite', 'wb') as f:
     f.write(tflite_model)
+
+
+
